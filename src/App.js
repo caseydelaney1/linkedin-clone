b5fabc6dcdd713a988af80f8f574ef6209c2309b
caseydelaney1 <caseydelaney2@gmail.com>
2021-12-21 07:56:41 -0500

    Commenting out Firebase dependency until I can figure out why it is blanking my project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { login, logout, selectUser } from './features/userSlice';
-import { useSelector, connect } from 'react-redux';
+import { useSelector } from 'react-redux';
 import './App.css';
 import Header from './Header';
 import Sidebar from './Sidebar';
@@ -9,7 +9,7 @@ import Login from './Login';
 
 function App() {
 
-  const user = useSelector(selectUser);
+  // const user = useSelector(selectUser);
 
   return (
     <div className="app">
@@ -17,13 +17,13 @@ function App() {
 {/* { header} */}
 <Header />
 
-{!user ? (<Login />) : (
+{/* {!user ? (<Login />) : ( */}
 <div className='app__body'>
   <Sidebar/>
     <Feed/>
   {/* widgets */}
 </div>
-)}
+{/* )} */}
     
 
     </div>
